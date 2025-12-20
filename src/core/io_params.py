9d5719fb979eada121a1a9ded38bb8e1a4b8c9a5
tks18 <tksudharshan@gmail.com>
2025-12-20 21:35:15 +0530

    feat(types): write all the io types required for input and export types

---
+++ src/core/io_params.py
@@ -0,0 +1,56 @@
+from typing import List, Literal, Optional
+from pydantic import BaseModel, Field
+
+# --- LOADERS ---
+
+
+class FileLoaderParams(BaseModel):
+    path: str
+    sheet: str = "Sheet1"
+    alias: str
+
+
+class SqlLoaderParams(BaseModel):
+    conn: str
+    query: str
+    alias: str
+
+
+class ApiLoaderParams(BaseModel):
+    url: str
+    alias: str
+
+# --- EXPORTERS ---
+
+
+class ParquetExportParams(BaseModel):
+    path: str = "output.parquet"
+    compression: Literal['snappy', 'zstd', 'gzip',
+                         'lz4', 'uncompressed', 'brotli'] = "snappy"
+
+
+class CsvExportParams(BaseModel):
+    path: str = "output.csv"
+
+
+class ExcelExportParams(BaseModel):
+    path: str = "output.xlsx"
+
+
+class JsonExportParams(BaseModel):
+    path: str = "output.json"
+
+
+class IpcExportParams(BaseModel):
+    path: str = "output.arrow"
+    compression: Literal['uncompressed', 'lz4', 'zstd'] = "uncompressed"
+
+
+class NdjsonExportParams(BaseModel):
+    path: str = "output.jsonl"
+
+
+class SqliteExportParams(BaseModel):
+    path: str = "output.db"
+    table: str = "data"
+    if_exists: Literal['fail', 'replace', 'append'] = "replace"
