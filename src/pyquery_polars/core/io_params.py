15e46263ceadecec0cd4c3b9c9fa43b54fe7ff23
tks18 <tksudharshan@gmail.com>
2026-01-10 11:23:28 +0530

    feat(models): update io_params to include source_info param for input and export_individual for exp

--- src/pyquery_polars/core/io_params.py
+++ src/pyquery_polars/core/io_params.py
@@ -9,6 +9,7 @@ class FileLoaderParams(BaseModel):
     sheet: str = "Sheet1"
     alias: str
     process_individual: bool = False  # Process files individually then concat
+    include_source_info: bool = False  # Add source metadata columns
 
 
 class SqlLoaderParams(BaseModel):
@@ -28,27 +29,33 @@ class ParquetExportParams(BaseModel):
     path: str = "output.parquet"
     compression: Literal['snappy', 'zstd', 'gzip',
                          'lz4', 'uncompressed', 'brotli'] = "snappy"
+    export_individual: bool = False
 
 
 class CsvExportParams(BaseModel):
     path: str = "output.csv"
+    export_individual: bool = False
 
 
 class ExcelExportParams(BaseModel):
     path: str = "output.xlsx"
+    export_individual: bool = False
 
 
 class JsonExportParams(BaseModel):
     path: str = "output.json"
+    export_individual: bool = False
 
 
 class IpcExportParams(BaseModel):
     path: str = "output.arrow"
     compression: Literal['uncompressed', 'lz4', 'zstd'] = "uncompressed"
+    export_individual: bool = False
 
 
 class NdjsonExportParams(BaseModel):
     path: str = "output.jsonl"
+    export_individual: bool = False
 
 
 class SqliteExportParams(BaseModel):
