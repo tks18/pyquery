7c978686d28b44d1c3c56a2d2238b3d28f9e6421
tks18 <tksudharshan@gmail.com>
2026-01-26 23:21:15 +0530

    feat(frontend/components): profile: create a class based component

---
+++ src/pyquery_polars/frontend/components/profile/__init__.py
@@ -0,0 +1,9 @@
+"""
+Profile component module.
+
+Provides dataset profiling functionality.
+"""
+
+from pyquery_polars.frontend.components.profile.__ui__ import ProfileComponent
+
+__all__ = ["ProfileComponent"]
