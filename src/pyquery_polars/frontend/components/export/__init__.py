572fb092900a80b0d62ae41c50421cc0c3043bd8
tks18 <tksudharshan@gmail.com>
2026-01-26 23:20:56 +0530

    feat(frontend/components): export: create a class based component

---
+++ src/pyquery_polars/frontend/components/export/__init__.py
@@ -0,0 +1,9 @@
+"""
+Export component module.
+
+Provides data export functionality for various formats.
+"""
+
+from pyquery_polars.frontend.components.export.__ui__ import ExportComponent
+
+__all__ = ["ExportComponent"]
