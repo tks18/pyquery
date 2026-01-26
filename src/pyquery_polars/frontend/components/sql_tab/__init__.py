044d23f610fc193b55e51806a1d6a20a416114c4
tks18 <tksudharshan@gmail.com>
2026-01-26 23:19:41 +0530

    feat(frontend/components): sql_tab: create a class based component

---
+++ src/pyquery_polars/frontend/components/sql_tab/__init__.py
@@ -0,0 +1,9 @@
+"""
+SQL Tab component module.
+
+Provides SQL query editor and execution functionality.
+"""
+
+from pyquery_polars.frontend.components.sql_tab.__ui__ import SQLTabComponent
+
+__all__ = ["SQLTabComponent"]
