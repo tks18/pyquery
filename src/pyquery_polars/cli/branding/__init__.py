7de839da3fbdb6cf7276be6457d9bbaf2158585e
tks18 <tksudharshan@gmail.com>
2026-01-27 08:56:30 +0530

    refactor(cli/branding): refactor branding to multiple sub modules for development exp

---
+++ src/pyquery_polars/cli/branding/__init__.py
@@ -0,0 +1,9 @@
+from .theme_manager import ThemeManager
+from .console_ui import ConsoleUI
+from .banner import BannerRenderer
+
+__all__ = [
+    "ThemeManager",
+    "ConsoleUI",
+    "BannerRenderer"
+]
